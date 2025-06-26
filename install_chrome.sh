394380c7027503df24608e9a8f51b91a3405ae55
Levi Goossens <levi.goossens107gmail.com>
2025-06-26 21:50:45 +0200

    add scripts for installing chrome and nodejs

---
+++ install_chrome.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# sudo apt --fix-broken install
+
+google-chrome-stable # launch chrome
