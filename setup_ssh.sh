657dbdbf79ed92f37b1b87fd93f337c8f6eb614d
Goossens Levi <Levi.Goossens@student.pxl.be>
2025-08-24 18:40:18 +0200

    Update setup_ssh.sh

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
+# requires copyq
+echo "setup ssh"
 
-mkdir ~/.ssh
-cd ~/.ssh
+cd .ssh/
+touch config
 
-ssh-keygen -P "" # ssh key without password
+echo "(1) github"
+echo "(2) gitlab"
+read -p "choose one of the given services above: " service
+
+if [ $service="github" ] || [ $service="1" ]; then
+  service="github"
+  hostname="github.com"
+elif [ $service="gitlab" ] || [ $service="2" ]; then
+  service="gitlab"
+  hostname="gitlab.com"
+else
+  echo "Error: service \"$service\" not listed"
+  exit 1
+fi
+
+read -p "enter username: " username
+
+filename="$service-$username"
+ssh-keygen -f $filename
+
+copyq add "$(cat "$filename.pub")"
+copyq select 0
+echo "add ssh-key on clipboard to your service or its account"
+
+echo "Host $service-$username\n
+    HostName $hostname\n
+    User $username\n
+    IdentityFile ~/.ssh/$filename" >> config
+
+echo "ssh-key setup for $filename"
