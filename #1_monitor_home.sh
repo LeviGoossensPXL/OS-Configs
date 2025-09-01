f82a45abd027f84482dc787a2e602292fbfa5a77
Levi Goossens <levigoossesn17@gmail.com>
2025-09-01 21:45:52 +0200

    configuration command for monitors at home

---
+++ #1_monitor_home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output DVI-I-5-4 --off --output DVI-I-4-3 --off --output DVI-I-2-1 --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1-0 --primary --mode 1920x1080 --pos 6000x0 --rotate normal --output HDMI-A-1-0 --off --output DVI-I-3-2 --mode 3440x1440 --pos 2560x0 --rotate normal
