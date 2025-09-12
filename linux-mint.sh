10f8d7a08c8cc2a63220cd6b4eecd4696416fd22
Levi Goossens <levi.goossens107@gmail.com>
2025-09-12 21:12:03 +0200

    update

--- linux-mint.sh
+++ linux-mint.sh
@@ -25,12 +25,12 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 #sudo apt install flatpak -y
 #flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 #
-#flatpak install flathub org.gnome.Mines
+#flatpak install flathub org.gnome.Mines #
 #flatpak install flathub com.mojang.Minecraft
 #flatpak install flathub com.github.avojak.warble
-#flatpak install flathub com.github.Anuken.Mindustry
+#flatpak install flathub com.github.Anuken.Mindustry #
 #flatpak install flathub in.p1x.TanksOfFreedom
-#flatpak install flathub com.getpostman.Postman
+#flatpak install flathub com.getpostman.Postman #
 #flatpak install flathub com.github.johnfactotum.QuickLookup
 #flatpak install flathub com.lynnmichaelmartin.TimeTracker
 #flatpak install flathub eu.nokun.MirrorHall
