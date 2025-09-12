b0f5203ee7798ef3eba5c928f225b83851648249
Levi Goossens <levi.goossens107@gmail.com>
2025-09-12 13:53:35 +0200

    rename files

--- install_fastfetch.sh
+++ install_fastfetch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 curl -OL https://github.com/fastfetch-cli/fastfetch/releases/download/2.51.1/fastfetch-linux-amd64.deb
 sudo apt install ./fastfetch-linux-amd64.deb
 sudo apt install fastfetch
+rm ./fastfetch-linux-amd64.deb
